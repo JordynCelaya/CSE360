e22cf938b2732f138b73ec9a5153101e3558635d
Jordyn <jordyncelaya@gmail.com>
2018-10-10 18:27:36 -0700

    Paths are now in descreasing order, Standalone error fixed

--- activityPackage/exceptions/CycleException.java
+++
@@ -1,5 +0,0 @@
-package activityPackage.exceptions;
-
-public class CycleException extends Exception {
-
-}
--- activityPackage/exceptions/StandaloneNodeException.java
+++
@@ -1,5 +0,0 @@
-package activityPackage.exceptions;
-
-public class StandaloneNodeException extends Exception {
-
-}
